a967f538bbfc22b2c24ee60aa4a3e2af29bf29d1
LSJ010427 <79030760+LSJ010427@users.noreply.github.com>
2022-03-11 17:14:00 +0900

    Add files via upload

---
+++ C++_ESPRESSO_programming_6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name, work;
+
+    cout << "당신의 이름을 입력하시오 : ";
+    cin >> name;
+    
+    cout << name << "씨 무엇을 해드릴까요?" << endl;
+
+    cout << "할 일을 입력하시오 : ";
+    cin >> work;
+
+    cout << work << "는 할 수 없습니다." << endl;
+
+    return 0;
+}
